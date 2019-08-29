5ede94e7f9b7d613c9500723069f109f3f20a133
Anthony Oglesby <ogle1ant@gmail.com>
2019-08-29 20:34:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,3 +23,15 @@
 #     deli.shift
 #   end
 # end
+
+
+def line(deli)
+  if deli.empty?
+    puts "This line is currently empty"
+  else
+    current_line = "The line is currently:"
+    deli.each.with_index(1) do |person, i|
+      current_line << "#{i}. #{person}"
+    end
+    puts current_line
+  end
